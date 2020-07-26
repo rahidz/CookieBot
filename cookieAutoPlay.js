5d1e2f104c221d07a44e20123cdb38a00e0a90de
rahidz <rahidz2003@gmail.com>
2020-07-26 04:35:56 -0400

    Update cookieAutoPlay.js
    oops

--- cookieAutoPlay.js
+++ cookieAutoPlay.js
@@ -2,9 +2,9 @@
 // Here, we just find the last stable release and load it.
 console.log("### COOKIE clicker STABLE - linking to latest release"); 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/repos/prinzstani/CookieBot/releases/latest', false);
+xhr.open('GET', 'https://api.github.com/repos/rahidz/CookieBot/releases/latest', false);
 xhr.send();
 var response = JSON.parse(xhr.responseText);
-var latest = "https://cdn.rawgit.com/prinzstani/CookieBot/" + response.tag_name + "/cookieAutoPlayBeta.js"
+var latest = "https://cdn.rawgit.com/rahidz/CookieBot/" + response.tag_name + "/cookieAutoPlayBeta.js"
 console.log("### LOADING FROM " + latest); 
 Game.LoadMod(latest);
